3b315b438c509a0feecc536957b72538b1835028
Eli Ribble <eli@authentise.com>
2015-10-01 10:49:01 -0600

    Only isort python source files
    It doesn't make sense for other types of files

--- mmiisort/main.py
+++ mmiisort/main.py
@@ -1,6 +1,7 @@
 from isort import SortImports
 import mothermayi.colors
 import mothermayi.errors
+import mothermayi.files
 
 def plugin():
     return {
@@ -16,9 +17,13 @@ def get_status(had_changes):
     return mothermayi.colors.red('unsorted') if had_changes else mothermayi.colors.green('sorted')
 
 def pre_commit(config, staged):
-    changes = [do_sort(filename) for filename in staged]
+    python_files = list(mothermayi.files.python_source(staged))
+    if not python_files:
+        return
+
+    changes = [do_sort(filename) for filename in python_files]
     messages = [get_status(had_change) for had_change in changes]
-    lines = ["  {0:<30} ... {1:<10}".format(filename, message) for filename, message in zip(staged, messages)]
+    lines = ["  {0:<30} ... {1:<10}".format(filename, message) for filename, message in zip(python_files, messages)]
     result = "\n".join(lines)
     if any(changes):
         raise mothermayi.errors.FailHook(result)
